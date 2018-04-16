993e4354c0ea49378c02b2d4059051fd38f07eab
Santhos <santa@Santhoss-MacBook-Pro.local>
2018-04-16 21:35:06 +0530

    updated 0.02

--- form/connect_reg.php
+++ form/connect_reg.php
@@ -1,5 +1,11 @@
 <?php
 
+    $username = 'root';
+    $password = 'root';
+    $dbname = 'Hostel';
+    $servername = 'localhost';
+    $conn=mysqli_connect($servername,$username,$password,$dbname);
+
     $first_name=$_POST['first_name'];
     $last_name=$_POST['last_name'];
     $reg_no=$_POST['reg_no'];
@@ -17,6 +23,19 @@
     $guardian_phone=$_POST['guardian_phone'];
     $room_no=$_POST['room_no'];
 
-    echo $first_name;
+    if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "INSERT INTO student (reg_no, first_name, last_name, date_of_birth, phone_no, email, blood_group, medical_conditions, street, city, state, pin, guardian_name, guardian_phone )
+    VALUES ('$reg_no', '$first_name', '$last_name', '$date_of_birth', '$phone_no', '$email', '$blood_group', '$medical_conditions', '$street', '$city', '$state', '$pin', '$guardian_name', '$guardian_phone')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    mysqli_close($conn);
 
 ?>
