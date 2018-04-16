d5a4b5371c0bde781ecdd5ae72d81d375828fdc0
krish276 <35499946+krish276@users.noreply.github.com>
2018-04-16 07:49:10 -0700

    Add files via upload

---
+++ form/connect_reg.php
@@ -0,0 +1,26 @@
+<?php
+    
+    $first_name=$_POST['first_name'];
+    $last_name=$_POST['last_name'];
+    $reg_no=$_POST['reg_no'];
+    $room_no=$_POST['room_no'];
+    $date_of_birth=$_POST['date_of_birth'];
+    $phone_no=$_POST['phone_no'];
+    $email=$_POST['email'];
+    $street=$_POST['street'];
+    $city=$_POST['city'];
+    $state=$_POST['state'];
+    $pin=$_POST['pin'];
+    $blood_group=$_POST['blood_group'];
+    $medical_conditions=$_POST['medical_conditions'];
+    $guardian_name=$_POST['guardian_name'];
+    $guardian_phone=$_POST['guardian_phone'];
+    $room_no=$_POST['room_no'];
+    
+    
+    
+  
+    
+    
+    
+    ?>
